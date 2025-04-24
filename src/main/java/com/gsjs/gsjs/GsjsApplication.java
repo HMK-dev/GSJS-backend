2bef7f9369663d31369d7f1064bd61837ae5be03
hamlsy <dltmddud1122@naver.com>
2025-04-24 13:13:29 +0900

    init

---
+++ src/main/java/com/gsjs/gsjs/GsjsApplication.java
@@ -0,0 +1,13 @@
+package com.gsjs.gsjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsjs/gsjs/GsjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsjs.gsjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
