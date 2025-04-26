3165180ff8dedad48dc68755c310f08a9ca126ca
hamlsy <dltmddud1122@naver.com>
2025-04-26 15:29:11 +0900

    feat: add scrap entity, repo #5

---
+++ src/main/java/com/gsjs/gsjs/domain/scrap/entity/Scrap.java
@@ -0,0 +1,32 @@
+package com.gsjs.gsjs.domain.scrap.entity;
+
+import com.gsjs.gsjs.domain.auditing.entity.BaseTimeEntity;
+import com.gsjs.gsjs.domain.company.entity.Company;
+import com.gsjs.gsjs.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Scrap extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+}
---
+++ src/main/java/com/gsjs/gsjs/domain/scrap/repository/ScrapRepository.java
@@ -0,0 +1,7 @@
+package com.gsjs.gsjs.domain.scrap.repository;
+
+import com.gsjs.gsjs.domain.scrap.entity.Scrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+}
